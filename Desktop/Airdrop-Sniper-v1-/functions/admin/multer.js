30619deb121289b6a149b1e0238ef27fbebef4a1
unknown <--global>
2023-10-11 18:03:30 +0300

    Airdrop Sniper V1

---
+++ Desktop/Airdrop-Sniper-v1-/functions/admin/multer.js
@@ -0,0 +1,28 @@
+const Mystorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../www/home/coinimages"));
+  },
+  filename: (req, file, cb) => {
+    const min = 10000;
+    const max = 1000000;
+    const randomInt = Math.floor(Math.random() * (max - min + 1) + min);
+    cb(null, randomInt + "" + path.extname(file.originalname));
+  },
+});
+
+const resimFileFilter = (req, file, cb) => {
+  if (
+    file.mimetype == "image/jpeg" ||
+    file.mimetype == "image/png" ||
+    file.mimetype == "image/svg+xml" ||
+    file.mimetype == "image/gif"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const uploadResim = multer({ storage: Mystorage, fileFilter: resimFileFilter });
+
+module.exports = uploadResim;
